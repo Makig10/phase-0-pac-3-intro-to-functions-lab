1bdeceb42d82e034975ef7407c2d5f0d69958d56
Brian <mahuibrian14@gmail.com>
2023-03-21 08:31:09 +0300

    1st commit

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase()) {
+    return "I can't hear you!" };
+    if(string.toUpperCase()) {
+    return "YES INDEED!";}
+    if(sayHiToHeadphonedRoommate("Let's have dinner together!")) {
+       return "I would love to!"}
+    }
+
+
+function sayHiToHeadphonedRoommate(string) {
+     if(string.toUpperCase()) {
+        return "YES INDEED!";}
+     if (string.toLowerCase()) {
+        return "I can't hear you!" };
+     if(sayHiToHeadphonedRoommate("Let's have dinner together!")) {
+           return "I would love to!"}
+ }
+ 
+
+
+
+
+
+
+    
